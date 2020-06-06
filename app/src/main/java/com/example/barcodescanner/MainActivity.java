a557b0a14a6539b8d02831bb32bd367b3507c30c
hp <chhabraashish123@gmail.com>
2020-06-06 19:45:25 +0530

    FireBase Updated

--- app/src/main/java/com/example/barcodescanner/MainActivity.java
+++ app/src/main/java/com/example/barcodescanner/MainActivity.java
@@ -1,15 +1,55 @@
 package com.example.barcodescanner;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView i;
+    Button b;
+    TextView t;
+    public static final int Pic_image=121;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        i=(ImageView)findViewById(R.id.image);
+        b=(Button)findViewById(R.id.choose);
+        t=(TextView)findViewById(R.id.text);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Image"),Pic_image);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==Pic_image) {
+            i.setImageURI(data.getData());
+            FirebaseVisionImage image;
+            try {
+                image = FirebaseVisionImage.fromFilePath(getApplicationContext(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
