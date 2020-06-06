c629364ff1965fb08c792368c922f46bd412bd82
hp <chhabraashish123@gmail.com>
2020-06-06 20:23:51 +0530

    MainActivity Updated

--- app/src/main/java/com/example/barcodescanner/MainActivity.java
+++ app/src/main/java/com/example/barcodescanner/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.barcodescanner;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 
 import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,6 +58,64 @@ public class MainActivity extends AppCompatActivity {
             FirebaseVisionImage image;
             try {
                 image = FirebaseVisionImage.fromFilePath(getApplicationContext(), data.getData());
+                FirebaseVisionBarcodeDetectorOptions options =
+                        new FirebaseVisionBarcodeDetectorOptions.Builder()
+                                .setBarcodeFormats(
+                                        FirebaseVisionBarcode.FORMAT_QR_CODE,
+                                        FirebaseVisionBarcode.FORMAT_AZTEC)
+                                .build();
+                FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance()
+                        .getVisionBarcodeDetector(options);
+
+                Task<List<FirebaseVisionBarcode>> result = detector.detectInImage(image)
+                        .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+                            @Override
+                            public void onSuccess(List<FirebaseVisionBarcode> barcodes) {
+                                // Task completed successfully
+                                // ...
+                                for (FirebaseVisionBarcode barcode: barcodes) {
+                                    Rect bounds = barcode.getBoundingBox();
+                                    Point[] corners = barcode.getCornerPoints();
+
+                                    String rawValue = barcode.getRawValue();
+
+                                    int valueType = barcode.getValueType();
+                                    // See API reference for complete list of supported types
+                                    switch (valueType) {
+                                        case FirebaseVisionBarcode.TYPE_WIFI: {
+                                            String ssid = barcode.getWifi().getSsid();
+                                            String password = barcode.getWifi().getPassword();
+                                            int type = barcode.getWifi().getEncryptionType();
+                                            t.setText(password);
+                                            break;
+                                        }
+                                        case FirebaseVisionBarcode.TYPE_URL: {
+                                            String title = barcode.getUrl().getTitle();
+                                            String url = barcode.getUrl().getUrl();
+                                            t.setText(url);
+                                            break;
+                                        }
+                                        case FirebaseVisionBarcode.TYPE_PHONE:
+                                        {  t.setText(barcode.getPhone().getNumber());
+                                            break;}
+                                        case FirebaseVisionBarcode.TYPE_EMAIL:
+                                        {t.setText(barcode.getEmail().getAddress()+"\n"+barcode.getEmail().getSubject()+"\n"+barcode.getEmail().getBody());
+                                            break;}
+                                    }
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                // ...
+                            }
+                        });
+// Or, to specify the formats to recognize:
+// FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance()
+//        .getVisionBarcodeDetector(options);
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
